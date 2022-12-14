e337f1efc3873ca7f42a88d9e0342d16d73e1cb3
Vivek <vivekpankaj1@gmail.com>
2022-12-14 20:00:43 +0530

    stepper control added

--- scripts/servo1.py
+++ scripts/servo1.py
@@ -10,21 +10,24 @@ class Main:
         
         self.sub = rospy.Subscriber('imu_machine', Imu, self.callback)
         self.pub_servo1 = rospy.Publisher('servo1', Int16, queue_size=10)
-        self.pub_dc = rospy.Publisher('dcmotor', Int16, queue_size=10)
+        self.pub_dc = rospy.Publisher('stepper', Int16, queue_size=10)
         self.rate = rospy.Rate(10)
         self.angle = 0
+        self.s_angle = 0
     
     def callback(self, imu_h):
-        rospy.loginfo(f"imu_machine={imu_h}")
         #convert imu data to angle
         z = imu_h.orientation.z
         y = imu_h.orientation.y
-        self.dc_angle = int(125*(y-0.72) + 90)
+        s_angle = (125*(y-0.72) + 90)*(5/9)
+        self.s_angle = int(s_angle)           #stepper angle
         self.angle = int(125*(z-0.72) + 90)
+        rospy.loginfo(f"stepper:{self.s_angle}")
         
     def run(self):
         while not rospy.is_shutdown():
             self.pub_servo1.publish(self.angle)
+            self.pub_dc.publish(self.s_angle)
             self.rate.sleep()
 
 if __name__ == "__main__":
